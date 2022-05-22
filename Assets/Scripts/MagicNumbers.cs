164046e76ee499950cb0fe1472af2f5fdb7f6ef7
Maksim Anishchenko <maksimych60@gmail.com>
2022-05-23 00:12:29 +0300

    Add restart and add inactive buttons, when the game is over

--- Assets/Scripts/MagicNumbers.cs
+++ Assets/Scripts/MagicNumbers.cs
@@ -1,52 +1,66 @@
+using System;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
 public class MagicNumbers : MonoBehaviour
 {
     [SerializeField] private int _min;
     [SerializeField] private int _max;
-
     [SerializeField] private TextMeshProUGUI InfoLabel;
     [SerializeField] private TextMeshProUGUI GuessLabel;
+    [SerializeField] private TextMeshProUGUI CountAttemptsLabel;
     [SerializeField] private Button MoreButton;
     [SerializeField] private Button LessButton;
     [SerializeField] private Button FinishButton;
+    private int _countAttempts;
     private int _guess;
+    private int _minReset;
+    private int _maxReset;
 
     private void Start()
     {
+        _minReset = _min;
+        _maxReset = _max;
+        
         MoreButton.onClick.AddListener(MoreButtonClicked);
         LessButton.onClick.AddListener(LessButtonClicked);
         FinishButton.onClick.AddListener(FinishButtonClicked);
-
-        SetInfoText($"задагай число от {_min} до {_max}");
+        
+        SetInfoText($"Задагай число от {_min} до {_max}");
+        
         GuessNumber();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _min = _minReset;
+            _max = _maxReset;
+            _countAttempts = 0;
+            
+            FinishButton.interactable = true;
+            LessButton.interactable = true;
+            MoreButton.interactable = true;
+            
+            SetCountAttemptsText("");
+            
+            GuessNumber();
+        }
     }
 
     private void GuessNumber()
     {
         _guess = (_max + _min) / 2;
         SetGuessText($"Твое число {_guess}?");
+        _countAttempts += 1;
     }
-
-    private void SetInfoText(string text)
-    {
-        Debug.Log(text);
-
-        InfoLabel.text = text;
-    }
-
-    private void SetGuessText(string text)
-    {
-        Debug.Log(text);
-
-        GuessLabel.text = text;
-    }
-
+    
     private void MoreButtonClicked()
     {
-        Debug.Log("Число больше");
         _min = _guess;
         GuessNumber();
     }
@@ -54,15 +68,31 @@ public class MagicNumbers : MonoBehaviour
     private void FinishButtonClicked()
     {
         SetGuessText($"Победа! Твое число: {_guess}");
-        MoreButton.onClick.RemoveListener(MoreButtonClicked);
-        LessButton.onClick.RemoveListener(LessButtonClicked);
-        FinishButton.onClick.RemoveListener(FinishButtonClicked);
+        SetCountAttemptsText($"Attempts: {_countAttempts}");
+
+        FinishButton.interactable = false;
+        LessButton.interactable = false;
+        MoreButton.interactable = false;
     }
 
     private void LessButtonClicked()
     {
-        Debug.Log("Число меньше");
         _max = _guess;
         GuessNumber();
+    }
+    
+    private void SetInfoText(string text)
+    {
+        InfoLabel.text = text;
+    }
+
+    private void SetGuessText(string text)
+    {
+        GuessLabel.text = text;
+    }
+
+    private void SetCountAttemptsText(string text)
+    {
+        CountAttemptsLabel.text = text;
     }
 }
