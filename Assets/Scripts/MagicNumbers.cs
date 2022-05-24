656754b7d3d0f7497c31adab001c04bd2495882f
Maksim Anishchenko <maksimych60@gmail.com>
2022-05-24 12:54:45 +0300

    some cleaning

--- Assets/Scripts/MagicNumbers.cs
+++ Assets/Scripts/MagicNumbers.cs
@@ -1,8 +1,5 @@
-using System;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 using Button = UnityEngine.UI.Button;
 
 public class MagicNumbers : MonoBehaviour
